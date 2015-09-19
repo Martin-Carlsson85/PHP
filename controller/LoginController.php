3329931f44dadcdef0b5768b57ff5a53c04d3755
Martin Carlsson <mc22dg@student.lnu.se>
2015-09-19 11:48:56 +0000

    Started implementing session and cookie login

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -14,7 +14,23 @@ class logincontroller
     function run(){
         $isLoggedIn = $this->isLoggedIn();
         if(!$isLoggedIn && $this->doesTheUserWantToLogin()) {
-            //Here we should do all logic for trying to log in...
+            //Checking login with session
+            $sessionView = new \view\SessionView();
+            $sessionCred = $sessionView->tryGetLoginCredentials();
+            if($sessionCred != false) {
+                $this->model->CheckUsernamnandPassword(
+                    $sessionCred[\view\SessionView::$username], 
+                    $sessionCred[\view\SessionView::$password]);
+            }
+            
+            //Checking login with cookies
+            $cookieView = new \view\CookieView();
+            $cookieCred = $cookieView->tryGetLoginCredentials();
+            if($cookieCred != false) {
+                $this->model->CheckUsernamnandPassword(
+                    $cookieCred[\view\SessionView::$username], 
+                    $cookieCred[\view\SessionView::$password]);
+            }
         }
         
         //TODO: We might have to read isLoggedIn again...
@@ -39,7 +55,7 @@ class logincontroller
      */
     function doesTheUserWantToLogin(){  //Funktion som kollar om användare vill logga in
         //Villkor som hämtar ifrån view
-        return false;
+        return true;
         /*if($this->loginView->getlogin()){
             $this->username = $this->loginView->getRequestUserName();   //Hämtar användarnamn från view
             $this->password = $this->loginView->getRequestUserPassword(); //Hämtar lösenord från view
--- index.php
+++ index.php
@@ -6,6 +6,8 @@ require_once('controller/LoginController.php');
 require_once('view/LayoutView.php');
 require_once('view/LoginView.php');
 require_once('view/DateTimeView.php');
+require_once('view/SessionView.php');
+require_once('view/CookieView.php');
 
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
