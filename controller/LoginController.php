0b8e622dca8b806138852807fec62a0158bff9f0
Martin Carlsson <mc22dg@student.lnu.se>
2015-09-19 11:17:06 +0000

    Skeleton for implementing the login functionality created

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -12,16 +12,37 @@ namespace controller;
         }
         
         function run(){
+            $isLoggedIn = $this->isLoggedIn();
+            if(!$isLoggedIn && $this->doesTheUserWantToLogin()) {
+                //Here we should do all logic for trying to log in...
+            }
+            
+            //TODO: We might have to read isLoggedIn again...
             $dtv = new \DateTimeView();
             $lv = new \LoginView($this->model);
-            $this->view->render(false, $lv, $dtv); //controller
+            $this->view->render($isLoggedIn, $lv, $dtv); //controller
         }
         
+        /**
+         * Checks if the user is logged in.
+         * 
+         * @return boolean
+         */
+        function isLoggedIn(){
+            return false;
+        }
+        
+        /**
+         * Checks if the user wants to log in.
+         * 
+         * @return boolean
+         */
         function doesTheUserWantToLogin(){  //Funktion som kollar om användare vill logga in
             //Villkor som hämtar ifrån view
-            if($this->loginView->getlogin()){
+            return false;
+            /*if($this->loginView->getlogin()){
                 $this->username = $this->loginView->getRequestUserName();   //Hämtar användarnamn från view
                 $this->password = $this->loginView->getRequestUserPassword(); //Hämtar lösenord från view
-            }
+            }*/
     }
 }
